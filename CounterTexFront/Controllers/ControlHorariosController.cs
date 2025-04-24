b4df755abf406dc8e630fd5d73510aba5bf852d7
Joseasegura <joseavelinosegurabociga@gmail.com>
2025-04-24 17:35:20 -0500

    Se agregan controlador y vistas de ControlHorarios y ControlPagos

---
+++ CounterTexFront/Controllers/ControlHorariosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CounterTexFront.Controllers
+{
+    public class ControlHorariosController : Controller
+    {
+        // GET: ControlHorarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
