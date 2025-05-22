285cdc4123af0e5a57c72ed5e861477c511a98ce
AngelRamirez14 <angelyoloxds@gmail.com>
2025-05-22 14:57:37 -0500

    commit mensaje chat

--- CounterTexFront/Models/MensajeChat.cs
+++ CounterTexFront/Models/MensajeChat.cs
@@ -1,21 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace CounterTexFront.Models
 {
-	public class MensajeChat
-	{
-        [Required(ErrorMessage = "El texto del mensaje es obligatorio.")]
-        [StringLength(200, ErrorMessage = "El mensaje no puede exceder los 200 caracteres.")]
-        public string Texto { get; set; }
+    public class MensajeChatViewModel
+    {
+        public int Id { get; set; }
 
-        public string Remitente { get; set; } // "usuario" o "sistema"
+        [Display(Name = "Remitente")]
+        public int RemitenteId { get; set; }
 
-        public DateTime FechaHoraEnvio { get; set; } = DateTime.Now; // Valor predeterminado
+        public string RemitenteNombre { get; set; } // extra para mostrar el nombre
 
+        [Display(Name = "Destinatario")]
+        public int DestinatarioId { get; set; }
 
+        public string DestinatarioNombre { get; set; } // extra para mostrar el nombre
+
+        [Display(Name = "Fecha y Hora")]
+        public DateTime FechaHora { get; set; }
+
+        [Required(ErrorMessage = "El mensaje no puede estar vacío.")]
+        [StringLength(1000, ErrorMessage = "El mensaje no debe exceder los 1000 caracteres.")]
+        public string Mensaje { get; set; }
     }
 }
