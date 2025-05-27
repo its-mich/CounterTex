b4d3e2c01d975fc18551750e597a7e7d63196e78
Daniels404 <zapatadanelstiven1710s.c@gmail.com>
2025-05-27 00:42:43 -0500

    se agrego la vista de asignacion empleado

---
+++ CounterTexFront/Controllers/AsignacionEmpleadoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CounterTexFront.Controllers
+{
+    public class AsignacionEmpleadoController : Controller
+    {
+        // GET: AsignacionEmpleado
+        public ActionResult AsignacionEmpleado()
+        {
+            return View();
+        }
+    }
+}
+
+
